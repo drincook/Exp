4fd1e63e30ee00e0f061c668d589ce3968fe76d0
drincook <eldrincook@gmail.com>
2024-02-16 21:02:18 -0500

    cuartocomitfuturedrincook

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth, { AuthOptions } from "next-auth";
+/*import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prismadb";
@@ -17,4 +17,45 @@ export const authOptions: AuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST };*/
+
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prismadb";
+import type { Adapter } from "next-auth/adapters";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export default handler;
+
+export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    // Código para manejar el método GET
+    res.status(200).json({ message: "GET request handled successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  } // Código para manejar el método GET
+};
+
+export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Código para manejar el método POST
+  try {
+    // Código para manejar el método POST
+    res.status(200).json({ message: "POST request handled successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
