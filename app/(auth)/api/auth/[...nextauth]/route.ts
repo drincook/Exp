f185bc9cbd185b9578fe11e699889fee9a44aeb1
drincook <eldrincook@gmail.com>
2024-02-18 00:25:08 -0500

    comit11future/drincook

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -27,3 +27,15 @@ const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };*/
 
 //as Adapter,
+/*import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const session = await getServerSession(authOptions);
+
+  return NextResponse.json({
+    authenticated: !!session,
+    session,
+  });
+}*/
