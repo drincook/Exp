00b0d20b5284efef88c43a2dac2be02ec63729dd
drincook <eldrincook@gmail.com>
2024-02-19 11:36:18 -0500

    comit21future/drincook

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,10 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prismadb";
 
 const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as any,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
