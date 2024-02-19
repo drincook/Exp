a02bbb6e70aa74dfe58f73c279b436d19d46edb5
drincook <eldrincook@gmail.com>
2024-02-19 13:29:54 -0500

    comit22future/drincook

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prismadb";
