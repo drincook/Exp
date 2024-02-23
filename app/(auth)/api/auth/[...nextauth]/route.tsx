fb2d055ebb56929f19855d1ec5bc0d77fcec6757
drincook <eldrincook@gmail.com>
2024-02-23 12:06:14 -0500

    comit49future/drincook

--- app/(auth)/api/auth/[...nextauth]/route.tsx
+++ app/(auth)/api/auth/[...nextauth]/route.tsx
@@ -1,35 +1,48 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { NextApiHandler } from "next";
-import { NextApiRequest as Request } from "next";
+// Importa los tipos necesarios de Next.js
+import { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
+// Importa NextAuth
 import NextAuth from "next-auth";
+// Importa la función para obtener el usuario actual
 import getCurrentUser from "@/app/actions/getCurrentUser";
+// Importa las opciones de autenticación
 import { authOptions } from "../../../utils/authRoute";
 
+// Función asincrónica para obtener el usuario
 const getUser = async () => {
   const user = await getCurrentUser();
   return user;
 };
 
+// Inicializa NextAuth con las opciones de autenticación
 const handler = NextAuth(authOptions);
 
+// Sobrescribe el método `handle` de NextAuth para personalizar el manejo de la solicitud
 handler.handle = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
+    // Obtiene el usuario actual
     const user = await getUser();
+    // Si no hay usuario y la URL de la solicitud comienza con "/api", redirecciona a "/login"
     if (!user && req.url?.startsWith("/api")) {
       return res.redirect("/login");
     }
 
+    // Maneja la solicitud utilizando NextAuth
     return await handler(req, res);
   } catch (error) {
+    // Maneja los errores y devuelve una respuesta de error
     console.log("Error in nextauth handle", error);
     return res.status(500).send(error);
   }
 };
 
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+// Exporta la función `handler` como el controlador predeterminado de la API
+export default handler;
+
+// Exporta funciones nombradas para manejar los métodos HTTP GET y POST
+export const GET: NextApiHandler = async (req, res) => {
   return await handler.handle(req, res);
 };
 
-export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
+export const POST: NextApiHandler = async (req, res) => {
   return await handler.handle(req, res);
 };
--- components/ui/Map.tsx
+++ components/ui/Map.tsx
@@ -1,8 +1,9 @@
 const Map = () => {
   return (
     <>
-      <div style={{ width: "100%" }}>
+      <div className="map-container w-[100%]">
         <iframe
+          title="Mapa de ubicación en Barcelona, España"
           width="100%"
           height="500"
           src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=es&amp;q=Les%20Rambles,%201%20Barcelona,%20Spain+(Mi%20nombre%20de%20egocios)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
