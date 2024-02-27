bb1e7e21acc6765dda503bcb8843aee62948fc31
drincook <eldrincook@gmail.com>
2024-02-27 11:29:21 -0500

    comit71future/drincook

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -9,6 +9,7 @@ import { PostTypes } from "@/types/postTypes";
 import { formatDate } from "@/utils/formatDate";
 import Link from "next/link";
 
+//const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
 
 const getData = async (id: string) => {
