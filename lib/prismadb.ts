f51a176a1343ff5fe43c609546949faf2370cdc6
drincook <eldrincook@gmail.com>
2024-02-27 14:25:09 -0500

    comit72future/drincook

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -3,7 +3,15 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
+const client =
+  globalThis.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // Accediendo a la variable de entorno para la URL de la base de datos
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
